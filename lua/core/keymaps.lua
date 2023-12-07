c73acb256e7b7d4cc08fc3170e94dae7eb8cf070
Jonny Gamba <jonny.gamba@deliveryhero.com>
2023-12-07 14:31:41 -0500

    add basic keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,3 +1,10 @@
 vim.g.mapleader = ";"
 
-local keymap = vim.keymap
+local keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- General keymaps
+keymap("n", "<leader>ww", "<cmd>update<cr>", opts)
+keymap("n", "<leader>xx", "<cmd>x<cr>", opts)
+keymap("n", "<leader>qq", "<cmd>q!<cr>", opts)
+keymap("n", "<leader>vi", ":e ~/.config/nvim/init.lua<cr>", opts)
