f0bd3af730ff442784893c9aee1fc42de9529f03
Jonny Gamba <jonny.gamba@deliveryhero.com>
2023-12-08 18:45:45 -0500

    change colorscheme

--- lua/plugins/colorcheme.lua
+++ lua/plugins/colorcheme.lua
@@ -1,12 +1,12 @@
 return {
-  "glepnir/oceanic-material",
+  "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
   opts = {},
   config = function()
     vim.cmd([[
       set background=dark
-      colorscheme oceanic_material
+      colorscheme tokyonight-night
     ]])
   end,
 }
