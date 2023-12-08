414a3c1bfdfa0f48c2aa82651b37633e5d01754c
Jonny Gamba <jonny.gamba@deliveryhero.com>
2023-12-08 08:22:54 -0500

    add indent-blankline plugin

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,19 @@
+local highlight = {
+  "CursorColumn",
+  "Whitespace",
+}
+
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  event = 'VeryLazy',
+  main = "ibl",
+  opts = {
+    enabled = true,
+    indent = { highlight = highlight, char = "" },
+    whitespace = {
+        highlight = highlight,
+        remove_blankline_trail = false,
+    },
+    scope = { enabled = false },
+  },
+}
