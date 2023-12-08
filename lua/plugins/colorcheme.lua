a22934665ed1f331e8723cc3598c8d9615e649ed
Jonny Gamba <jonny.gamba@deliveryhero.com>
2023-12-08 13:11:33 -0500

    use solarized-osaka colorscheme

--- lua/plugins/colorcheme.lua
+++ lua/plugins/colorcheme.lua
@@ -1,17 +1,10 @@
 -- Theme/Colorscheme
 return {
-  "tiagovla/tokyodark.nvim",
-  lazy = false, -- We want the colorscheme to load immediately when starting Neovim
-  priority = 1000, -- Load the colorscheme before other non-lazy-loaded plugins
+  "craftzdog/solarized-osaka.nvim",
+  lazy = false,
+  priority = 1000,
   opts = {},
   config = function()
-     vim.cmd("colorscheme tokyodark")
-    -- Colorscheme overrides
-    vim.cmd([[
-      autocmd VimEnter * hi DiffAdd guifg=#00FF00 guibg=#005500
-      autocmd VimEnter * hi DiffDelete guifg=#FF0000 guibg=#550000
-      autocmd VimEnter * hi DiffChange guifg=#CCCCCC guibg=#555555
-      autocmd VimEnter * hi DiffText guifg=#00FF00 guibg=#005500
-    ]])
-  end
+    vim.cmd("colorscheme solarized-osaka")
+  end,
 }
