3590f20c5f3ffa612ac5bc6a7edbd95075a00718
Jonny Gamba <jonny.gamba@deliveryhero.com>
2023-12-08 10:37:38 -0500

    add tokyodark colorscheme

--- init.lua
+++ init.lua
@@ -20,7 +20,7 @@ vim.g.mapleader = ";"
 
 -- Initialize lazy with dynamic loading of anything in the plugins directory
 require("lazy").setup({{ import = "plugins" }, { import = "plugins.lsp" }}, {
-   change_detection = {
+  change_detection = {
     enabled = true, -- automatically check for config file changes and reload the ui
     notify = false, -- turn off notifications whenever plugin changes are made
   },
--- lua/plugins/colorcheme.lua
+++ lua/plugins/colorcheme.lua
@@ -1,17 +1,11 @@
 -- Theme/Colorscheme
 return {
-  'rebelot/kanagawa.nvim', -- You can replace this with your favorite colorscheme
+  "tiagovla/tokyodark.nvim",
   lazy = false, -- We want the colorscheme to load immediately when starting Neovim
   priority = 1000, -- Load the colorscheme before other non-lazy-loaded plugins
-  opts = {
-    -- Replace this with your scheme-specific settings or remove to use the defaults
-    -- background = {
-    --   dark = "wave",
-    -- },
-  },
-  config = function(_, opts)
-    require('kanagawa').setup(opts) -- Replace this with your favorite colorscheme
-    vim.cmd("colorscheme kanagawa") -- Replace this with your favorite colorscheme
+  opts = {},
+  config = function()
+     vim.cmd("colorscheme tokyodark")
     -- Colorscheme overrides
     vim.cmd([[
       autocmd VimEnter * hi DiffAdd guifg=#00FF00 guibg=#005500
