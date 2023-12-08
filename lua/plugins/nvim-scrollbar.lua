682aa9c50ffeeceef2c512ab6de21b6c0f6abaa5
Jonny Gamba <jonny.gamba@deliveryhero.com>
2023-12-08 12:03:56 -0500

    add nvim-scrollbar plugin

---
+++ lua/plugins/nvim-scrollbar.lua
@@ -0,0 +1,12 @@
+return {
+  "petertriho/nvim-scrollbar",
+  name = "scrollbar",
+  opts = {
+    handlers = { gitsigns = true },
+    marks = {
+      GitAdd = { color = "#266d6a" },
+      GitChange = { color = "#536c9e" },
+      GitDelete = { color = "#b2555b" },
+    },
+  },
+}
