b0fc03cd1cb03c43bebf5b145fe4bc7b6f3b082e
Jonny Gamba <jonny.gamba@deliveryhero.com>
2023-12-09 13:27:44 -0500

    update telescope

--- lua/plugins/telescope/telescope.lua
+++ lua/plugins/telescope/telescope.lua
@@ -9,7 +9,6 @@ local git_icons = {
 
 return {
   "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
   dependencies = { { "nvim-lua/plenary.nvim" } },
   config = function()
     local actions = require("telescope.actions")
