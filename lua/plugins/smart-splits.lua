b1996e624f828f8ecaebfa19eab7c17f2f826092
Jonny Gamba <jonny.gamba@deliveryhero.com>
2024-02-23 14:33:00 +0100

    remove bufresize plugin

--- lua/plugins/smart-splits.lua
+++ lua/plugins/smart-splits.lua
@@ -1,11 +1,4 @@
 return {
   "mrjones2014/smart-splits.nvim",
   config = true,
-  opts = {
-    resize_mode = {
-      hooks = {
-        on_leave = require("bufresize").register,
-      },
-    },
-  },
 }
